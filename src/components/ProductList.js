d2f988e5d2cb744b3b9cd2a48e3d4b95ea099195
“elizanort” <elizabethnortman@gmail.com>
2020-06-02 08:32:22 -0500

    activity completed

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,17 +1,27 @@
 import React from "react";
 import ProductSummary from "./ProductSummary";
+import ProductDetail from "./ProductDetail";
+import {Switch, Route, withRouter} from 'react-router-dom';
+
 
 function ProductList(props) {
   return (
-    <div className="productList">
-      <h2>Our Products</h2>
-      <ul>
-        {props.productList.map((product) => (
-          <ProductSummary key={product.id} product={product} />
-        ))}
-      </ul>
-    </div>
+    <Switch>
+      <Route path="/products/:productId">
+        <ProductDetail productList={props.productList}/>
+      </Route>
+      <Route path="/products">
+        <div className="productList">
+          <h2>Our Products</h2>
+          <ul>
+            {props.productList.map((product) => (
+              <ProductSummary key={product.id} product={product} />
+            ))}
+          </ul>
+        </div>
+      </Route>
+    </Switch>
   );
 }
 
-export default ProductList;
+export default withRouter(ProductList);
